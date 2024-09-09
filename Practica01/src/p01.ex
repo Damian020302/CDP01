15ed6361e624361d98319f01954c905e43147137
Santi24Yt <santi24yt@gmail.com>
2024-09-09 01:27:06 -0600

    ejercicios bonitos

--- Practica01/src/p01.ex
+++ Practica01/src/p01.ex
@@ -9,115 +9,166 @@ defmodule P01 do
   @doc """
   Calcula el cuádruple de un número dado
   """
-  def cuadruple(num) when is_number(num) do
-    num * 4
-  end
+  def cuadruple(n) when is_number(n), do: n * 4
 
   @doc """
   Devuelve el sucesor un número
   """
-  def sucesor(num) when is_number(num) do
-    num + 1
-  end
+  def sucesor(n) when is_number(n), do: n + 1
 
   @doc """
   Encuentra el máximo entre dos números
   """
-  def maximo(num1, num2) when is_number(num1) and is_number(num2) do
-    if num1 > num2 do
-      num1
-    else
-      num2
-    end
-  end
+  def maximo(n1, n2) when is_number(n1) and is_number(n2), do: (if n1 > n2, do: n1, else: n2)
 
   @doc """
   Suma dos números
   """
-  def suma(num1, num2) when is_number(num1) and is_number(num2) do
-    num1 + num2
-  end
+  def suma(n1, n2) when is_number(n1) and is_number(n2), do: n1 + n2
 
   @doc """
   Resta el segundo número del primero
   """
-  def resta(a, b) when is_number(a) and is_number(b) do
-    a - b
-  end
+  def resta(n1, n2) when is_number(n1) and is_number(n2), do: n1 - n2
 
   @doc """
   Multiplica la multiplicación de conjugados
   """
-  def multiplicacionConjugados(a, b) when is_number(a) and is_number(b) do
-    a * a - b * b
-  end
+  def multiplicacionConjugados(n1, n2) when is_number(n1) and is_number(n2), do: n1*n1 - n2*n2
 
   @doc """
   Devuelve la negación de un valor booleano
   """
-  def negacion(bool) when is_boolean(bool) do
-    not bool
-  end
+  def negacion(true), do: false
+  def negacion(false), do: true
 
   @doc """
   Realiza la conjunción lógica entre dos valores booleanos
   """
-  def conjuncion(bool1, bool2) when is_boolean(bool1) and is_boolean(bool2) do
-    bool1 and bool2
-  end
+  def conjuncion(true, true), do: true
+  def conjuncion(_, _), do: false
 
   @doc """
   Realiza la disyunción lógica entre dos valores booleanos
   """
-  def disyuncion(bool1, bool2) when is_boolean(bool1) and is_boolean(bool2) do
-    bool1 or bool2
-  end
+  def disyuncion(false, false), do: false
+  def disyuncion(_, _), do: true
 
   @doc """
   Calcula el valor obsoluto de un número
   """
-  def absoluto(num) when is_number(num) do
-    if num < 0 do
-      -num
-    else
-      num
-    end
-  end
+  def absoluto(n) when is_number(n), do: (if n >= 0, do: n, else: -n)
 
   @doc """
   Calcula el área de un círculo dado su radio
   """
-  def areaCirculo(r) when is_number(r) do
-    3.14 * r * r
-  end
+  def areaCirculo(r) when is_number(r), do: 3.14 * r*r
 
   @doc """
   Calcula la suma de Gauss de manera recursiva
   """
-  def sumaGaussRec(n) when is_integer(n) and n >= 0 do
-    if n == 0 do
-      0
-    else
-      n + sumaGaussRec(n - 1)
-    end
-  end
+  def sumaGaussRec(0), do: 0
+  def sumaGaussRec(n) when is_number(n) and n >= 0, do: n + sumaGaussRec(n-1)
+  def sumaGaussRec(_), do: 0
 
   @doc """
   Calcula la suma de Gauss usando la fórmula cerrada
   """
-  def sumaGauss(n) when is_integer(n) and n >= 0 do
-    n * (n + 1) / 2
-  end
+  def sumaGauss(n) when is_number(n) and n >= 0, do: (n * (n+1)) / 2
+  def sumaGauss(_), do: 0
 
   @doc """
   Calcula el área de un triangulo dados tres puntos del plano
   """
-  def areaTriangulo(a, b, c) when is_tuple(a) and is_tuple(b) and is_tuple(c) do
-    {x1, y1} = a
-    {x2, y2} = b
-    {x3, y3} = c
-    abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2)
-  end
+  def areaTriangulo({x1, y1}, {x2, y2}, {x3, y3}), do: absoluto((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)) / 2)
+
+  @doc """
+  Dado un número n y una cadena regresa una lista con n veces la cadena
+  ### Parameters
+  - n: número de veces a repetir la cadena
+  - s: la cadena a repetir
+  """
+  def repiteCadena(0, _), do: []
+  def repiteCadena(n, s) when is_number(n), do: [ s | repiteCadena(n-1, s) ]
+
+  @doc """
+  Dada una lista, un índice i y un valor, regresa la lista con el valor insertado en el indice i de la lista.
+  ### Parameters
+  - l: la lista a la que se le va a insertar el elemento
+  - i: el indice donde se va a insertar el elemento en la lista
+  - e: el elemento que se va a insertar en el indice en la lista
+  """
+  def insertaElemento([], _, e), do: [e]
+  def insertaElemento(l, 0, e), do: [ e | l ]
+  def insertaElemento([h | t], i, e) when is_number(i), do: [ h | insertaElemento(t, i-1, e) ]
+
+  @doc """
+  Dada una lista y un índice i, regresa la lista sin el elemento en la posición i.
+  ### Parameters
+  - l: la lista de la cual se quiere eliminar un elemento
+  - i: el indice del elemento que se quiere eliminar de la lista
+  """
+  def eliminaIndex([], _), do: []
+  def eliminaIndex([_ | t], 0), do: t
+  def eliminaIndex([h | t], i) when is_number(i), do: [ h | eliminaIndex(t, i-1) ]
+
+  @doc """
+  Regresa el  ́ultimo elemento de una lista.
+  ### Parameters
+  - l: la lista de la cual se quiere obtener el ultimo elemento
+  """
+  def raboLista([e]), do: e
+  def raboLista([_ | t]), do: raboLista(t)
+
+  @doc """
+  Dada una lista de listas, encapsula en tuplas los elementos correspondientes de cada lista.
+  ### Parameters
+  - l: La lista de la cual se quiere encapsular en tuplas los elementos correspondientes de cada lista
+  """
+  def encapsula(l) when is_list(l), do: List.zip(l)
+
+  @doc """
+  Dado un map y una llave, regresa el map sin la entrada con la llave.
+  ### Parameters
+  - m: Map del que se quiere borrar un elemento
+  - k: La llave del elemento que se quiere borrar del map
+  """
+  def mapBorra(m, k) when is_map(m) and is_number(k), do: Map.delete(m, k)
+
+  @doc """
+  Dado un map, regresa su conversión a una lista.
+  ### Parameters
+  - m: El map que se quiere convertir a lista
+  """
+  def mapAlista(m) when is_map(m), do: Map.to_list(m)
+
+  @doc """
+  Calcula la distancia entre dos puntos.
+  ### Parameters
+  - p1: El punto inicial para calcular la distancia (tupla x,y)
+  - p2: El punto final para calcular la distancia (tupla x,y)
+  """
+  def dist({x1, y1}, {x2, y2}) when
+    is_number(x1) and
+    is_number(x2) and
+    is_number(y1) and
+    is_number(y2), 
+    do: ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+  @doc """
+  Inserta un elemento en una tupla.
+  ### Parameters
+  - t: Tupla a la cual se le quiere añadir un elemento
+  - e: Elemento que se quiere añadir a la tupla
+  """
+  def insertaTupla(t, e) when is_tuple(t), do: Tuple.append(t, e)
+
+  @doc """
+  Pasa de una tupla a una lista.
+  ### Parameters
+  - t: Tupla la cual se quiere pasar a una lista
+  """
+  def tuplaALista(t) when is_tuple(t), do: Tuple.to_list(t)
 
   # ---------------------------------------- Pruebas ----------------------------------------
   test "pruebaCuadruple" do
