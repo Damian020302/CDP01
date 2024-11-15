b6fb082087e768fffe544f3aa65e3afc971b4d04
Damian020302 <damianvazqueztorrijos@ciencias.unam.mx>
2024-11-15 17:52:29 -0600

    Modulos 'principales'

---
+++ Proyecto/src/crypto.ex
@@ -0,0 +1,31 @@
+defmodule Crypto do
+
+  @block_fields [:data, :timestamp, :prev_hash]
+
+  @doc "Calcula el has de un bloque"
+  def hash(%{} = block) do
+      block
+      |> Map.take(@block_fields)
+      |> encode_to_binary()
+      |> simple_hash()
+  end
+
+  @doc "Calcula e inserta el hash en el bloque"
+  def put_hash(%{} = block) do
+      %{block | hash: hash(block)}
+  end
+
+
+  @doc "Convierte un mapa a binario"
+  defp encode_to_binary(map) do
+      map
+      |> Enum.map(fn {_, value} -> "#{value}" end) # Convierte cada valor en string
+      |> Enum.join()                               # Concatena todos los valores en un solo string
+      |> :erlang.binary_to_list()                  # Convierte el string a binario
+  end
+
+  @doc "Calcula el hash de un binario"
+  defp simple_hash(binary) do
+      :erlang.phash2(binary) |> Integer.to_string(16)
+  end
+end
